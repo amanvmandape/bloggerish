23530d0707475b6c0c5447b8a69c0438ce847fc8
Armaan <amanvmandape@gmail.com>
2024-07-15 13:22:20 +0530

    Initial commit

---
+++ src/main/java/com/bloggerish/bloggerish/BloggerishApplication.java
@@ -0,0 +1,13 @@
+package com.bloggerish.bloggerish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloggerish/bloggerish/BloggerishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloggerish.bloggerish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
